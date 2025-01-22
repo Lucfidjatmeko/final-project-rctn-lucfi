7a6d8c52a4ad5e0a6d42793f927c6a84a0084254
Lucfi Djatmeko <planadmi1@toyota.co.id>
2025-01-22 15:16:52 +0700

    update

--- finalProjectRCTN/src/App.jsx
+++ finalProjectRCTN/src/App.jsx
@@ -1,23 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
- 
-import {Routes, Route} from "react-router-dom"
+// import './App.css'
 import HomePage from './pages/HomePage'
+import {Routes, Route} from "react-router-dom"
+import NavbarComponent from './ComponenPages/NavBarComponent'
 import DetailPage from './pages/DetailPage'
  
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
  
   return (
     <>
-     
-      <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/detail/:id' element={<DetailPage/>} />
+    {/* <HomePage/> */}
  
-      </Routes>
+<NavbarComponent/>
+ 
+    <Routes>
+       <Route path ='/' element={<HomePage/>} />
+       <Route path ='/detail/:id' element={<DetailPage/>} />
+    </Routes>
+   
     </>
   )
 }
